109e38fd6814c6a050c94d1fdcbad4b01a3b65f5
Mohammed Suleiman Mohamed Al-falahy <32552843+mamdinho@users.noreply.github.com>
2023-04-27 15:39:24 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // This is a placeholder function that runs when the page is loaded.
+  // You can add more interactive functionality using JavaScript and NodeJS here.
+
+  console.log('Page loaded and ready!');
+});
